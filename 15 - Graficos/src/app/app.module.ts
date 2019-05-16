183fc7f64fceebb3b8cc4ddb158333ba54117208
Alexander Nova <titoantifa69@gmail.com>
2019-05-16 00:44:01 -0500

    Add los graficos

---
+++ 15 - Graficos/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// Graficos
+import { ChartsModule } from 'ng2-charts';
+
+import { AppComponent } from './app.component';
+import { LineaComponent } from './components/linea/linea.component';
+import { BarraComponent } from './components/barra/barra.component';
+import { DonaComponent } from './components/dona/dona.component';
+import { RadarComponent } from './components/radar/radar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LineaComponent,
+    BarraComponent,
+    DonaComponent,
+    RadarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
